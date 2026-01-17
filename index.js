566fcb8a0a90364c234fbb9e35028d84efac3d09
23120020 <syynguyen999@gmail.com>
2026-01-17 10:02:31 +0700

    testing

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.json({
-    message: 'Hello World!',
+    message: 'Hello World! hi',
     timestamp: new Date()
   });
 });
